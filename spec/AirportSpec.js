415c79c4b4f40e147e9d26603f487b5cb6d8af21
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-26 15:53:06 +0000

    sets up initial stuff and adds test for hangar to airport

---
+++ spec/AirportSpec.js
@@ -0,0 +1,16 @@
+describe("Aiport", function() {
+
+  var airport;
+  var plane;
+
+  beforeEach(function() {
+    airport = new Airport();
+    plane = new Plane();
+  });
+
+  describe("Hangar", function() {
+    it("Has an empty hangar to start with", function() {
+      expect(airport.hangar()).toEqual([]);
+    });
+  });
+});
