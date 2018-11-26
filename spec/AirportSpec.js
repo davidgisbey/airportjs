b8435889921fd2c1ba88ca3452e615bea6a1c8b3
David Gisbey <davidgisbey@Davids-MacBook-Pro.local>
2018-11-26 16:37:29 +0000

    plane.land method implemented and pished into hangar

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,12 +1,19 @@
 describe("Airport", function() {
 
   var airport;
+  var plane;
 
   beforeEach(function() {
     airport = new Airport();
+    plane = jasmine.createSpy('plane',['land']);
   });
 
   it("Has an empty hangar to start with", function() {
     expect(airport.hangar()).toEqual([]);
+  });
+
+  it("can clear planes for landing", function() {
+    airport.clearForLanding(plane);
+    expect(airport.hangar()).toEqual([plane]);
   });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,4 +1,9 @@
 function Airport() {
-
+  this._hangar = [];
 }
-Airport.prototype.hangar = function() { return []; };
+
+Airport.prototype.hangar = function() { return this._hangar; };
+
+Airport.prototype.clearForLanding = function(plane) {
+  this._hangar.push(plane);
+  };
